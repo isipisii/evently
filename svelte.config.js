cd9efba087938881f62b2f11867d9376a850f560
Peter John Arao <pitzzahh@gmail.com>
2025-02-17 14:44:47 +0800

    fix: Update Svelte config for alias paths and add base path handling

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,8 +6,12 @@ const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter(),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		},
 		alias: {
-			"@/*": "./path/to/lib/*",
+			'@/*': './src/lib/*',
+			'@routes/*': './src/routes/*'
 		},
 	}
 };
